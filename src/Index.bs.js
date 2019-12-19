fca74721db909eb10e5ea8fd0bb733d87f6fad60
Eduardo Fernandes <failcake.work@gmail.com>
2019-12-19 08:51:43 +0000

    Removed generated files Adding heroku deployment

--- src/Index.bs.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var React = require("react");
-var ReactDOMRe = require("reason-react/src/ReactDOMRe.js");
-var App$Ubireason = require("./components/app/App.bs.js");
-
-ReactDOMRe.renderToElementWithId(React.createElement(App$Ubireason.make, { }), "app");
-
-/*  Not a pure module */
