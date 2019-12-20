86ab66aec867ac63b334b3ec66736aed74733ab8
Eduardo Fernandes <failcake.work@gmail.com>
2019-12-20 11:55:25 +0000

    a

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/Index.bs.js',
     mode: 'production',
     output: {
-    path: path.join(__dirname, '.bin'),
+        path: path.join(__dirname, '.bin'),
         filename: 'index.js',
-    }
+    },
+    plugins: [
+        new CopyPlugin([
+            { from: '../index_prod.html', to: '.' }
+        ]),
+    ],
 };
